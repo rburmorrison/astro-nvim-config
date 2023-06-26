ab1837c0a7c92f9695e070d09e023cf86384a54d
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2023-06-26 19:58:22 -0400

    Add black to auto-installed formatters

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -27,6 +27,7 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
+        "black",
         "prettier",
         "stylua",
         "shellcheck",
