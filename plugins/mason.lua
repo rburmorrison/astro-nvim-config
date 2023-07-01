b5284cb14691affe8df6bd9f0c042aed40fe23d4
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2023-07-01 10:21:52 -0400

    Remove pylint from the autoinstall list
    Removing this again because in virtual environments the autoinstalled pylint does not resolve package imports properly.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -27,7 +27,6 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
-        "pylint",
         "black",
         "prettier",
         "stylua",
